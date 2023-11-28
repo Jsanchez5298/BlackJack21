e6f6a167b198a26f37b4ccacac249bb36f84290d
Jsanchez5298 <70111838+Jsanchez5298@users.noreply.github.com>
2023-11-27 22:39:55 -0600

    Update player.py

--- player.py
+++ player.py
@@ -21,25 +21,6 @@ class Player:
 
         return hand_value   # Return the hand value
 
-    #Placeholder for player action handling. You might implement logic to handle 'hit' or 'stand' actions here.
-    #def take_action(self, mouse_pos, deck):
-        #Here you might implement how a player would decide to 'hit' or 'stand' based on game logic or user input
-        # For instance:
-        # while player_input != 'stand':
-        #    if player_input == 'hit':
-        #        self.draw_card(deck.deal_card())
-        #    player_input = input("Do you want to hit or stand? ")
-
-        #if self.hit_button.collidepoint(mouse_pos):
-           # self.player.draw_card(deck.deal_card())
-       # elif self.stand_button.collidepoint(mouse_pos):
-           # pass
-            # Player stands, move to dealer's turn
-            # Implement dealer's logic or update game state accordingly
-
-
-
-
     #Reset the player's hand for a new round.
     def reset_hand(self):
         self.hand = []
